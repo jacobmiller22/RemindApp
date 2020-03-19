3a06202d45bd7c03d65017469cdebd0ebb5a873e
Jacob Miller <jacobmiller22@vt.edu>
2020-03-19 15:15:08 -0400

    Initial Commit

---
+++ RemindApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemindApp
+//
+//  Created by Jacob Miller on 3/19/20.
+//  Copyright © 2020 Jacob Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
